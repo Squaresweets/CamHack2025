9a7c6fb30885817e28b52af0e5a4abb534e1d232
Duy Huynh <duy@donggiang.com>
2025-11-01 20:33:13 +0000

    Tweaked clock detection for mac

--- clock_detector.py
+++ clock_detector.py
@@ -40,7 +40,8 @@ class ClockDetector:
             raise ValueError("Invalid image provided.")
 
         image = cv2.cvtColor(np.array(_image), cv2.COLOR_RGB2BGR)
-
+        cv2.imwrite("clock_detector.png", image)
+        #print(image)
         # Convert to HSV color space
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
@@ -50,15 +51,26 @@ class ClockDetector:
         # lower_red2 = np.array([175, 190, 240])
         # upper_red2 = np.array([180, 210, 255])
 
-        lower_red1 = np.array([0, 190, 240])
-        upper_red1 = np.array([5, 210, 255])
-        lower_red2 = np.array([175, 190, 240])
-        upper_red2 = np.array([180, 210, 255])
+        # Tweaked for mac
+        lower_red1 = np.array([0, 210, 235])
+        upper_red1 = np.array([5, 255, 255])
+        lower_red2 = np.array([175, 210, 235])
+        upper_red2 = np.array([180, 255, 255])
 
         # Create masks for red
         mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
         mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
         red_mask = mask1 | mask2
+
+        # Prevent overtime detection
+        # Calculate bounds for the middle 95% of the width
+        image_width = image.shape[1]
+        lower_bound = int(image_width * 0.025)
+        upper_bound = int(image_width * 0.975)
+
+        # Zero out pixels outside the middle 95% of the width
+        red_mask[:, :lower_bound] = 0
+        red_mask[:, upper_bound:] = 0
 
         # Find contours in the mask
         contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
